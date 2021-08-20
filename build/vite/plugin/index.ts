71eaa78cb6dd83eb1b1b07eec535f17392b401de
vino <vino0908@outlook.com>
2021-08-20 14:48:22 +0800

    feat: 添加svg-icon

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -2,6 +2,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import vue from "@vitejs/plugin-vue";
 import {PluginOption} from "vite";
 import {configMockPlugin} from "./configMockPlugin";
+import {svgBuilder} from "./svgBuilder";
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
     const {VITE_USE_MOCK} = viteEnv;
@@ -9,6 +10,9 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
     const vitePlugins: (PluginOption | PluginOption[]) = [vue(), vueJsx()];
 
     VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild));
+
+    //添加svg-icon插件
+    vitePlugins.push(svgBuilder('./src/assets/icons/'));
 
     return vitePlugins;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import {createApp} from "vue";
 import App from "./App.vue";
 import {setupRouter} from "./router";
 import "/@/assets/index.scss";
+import SvgIcon from "./components/SvgIcon.vue";
 
 function bootstrap() {
     const app = createApp(App);
     setupRouter(app);
+    app.component('svg-icon', SvgIcon);
     app.mount("#app", true);
 }
 
